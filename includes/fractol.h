560c55d697f26d76e9f4f12376c21ecd20733986
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-12-26 18:41:47 +0200

    Add initial MLX42 setup

---
+++ includes/fractol.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 12:56:43 by phuocngu          #+#    #+#             */
+/*   Updated: 2024/12/26 18:33:03 by phuocngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# define WIDTH 800
+# define HEIGHT 800
+
+#include <stdbool.h>
+#include <MLX42/MLX42.h>
+
+#endif
